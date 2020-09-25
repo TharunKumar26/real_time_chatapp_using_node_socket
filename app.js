a29824e538bb318919d35e59713b0a0ed8ab8435
unknown <balarajutk26@gmail.com>
2020-09-25 13:57:21 +0530

    changes

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ io.on("connection",(socket)=>{
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 httpserver.listen(PORT, console.log(`Server started on port ${PORT}`));
