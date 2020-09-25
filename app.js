57a0a0e11bd63fa2cb04eed19b477bd663f01d3f
unknown <balarajutk26@gmail.com>
2020-09-25 13:44:39 +0530

    intitial

--- app.js
+++ app.js
@@ -5,16 +5,7 @@ const http = require('http');
 const socket = require('socket.io')
 
 const bodyParser = require('body-parser')
-/*
-const mongoose = require('mongoose');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
 
-const User = require('./models/user.model');
-const Admin = require('./models/admin');
-const Vendor = require('./models/Vendor');
-*/
 
 const app = express();
 var httpserver = http.createServer(app)
@@ -77,116 +68,6 @@ io.on("connection",(socket)=>{
 
 
 
-
-
-/*
-
-// Express Session
-app.use(session({
-    secret: 'secret', 
-    resave: false,
-    saveUninitialized: false,
-}));
-
-
-
-// Passport config
-require('./config/passport')(passport);
-
-
-
-// DB Config
-const db = require('./config/keys').mongoURI; 
-
-// Connect to Mongo
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then( () => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
-
-// EJS
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Connect flash
-app.use(flash());
-
-// User
-app.use((req, res, next) => {
-    if(!req.session.user){
-      return next();
-    }
-    User.findById(req.session.user._id)
-      .then(user => {
-        req.user = user;
-        next();
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  });
-  app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    next();
-  });
-
-  // Admin
-  app.use((req, res, next) => {
-    if(!req.session.user){
-      return next();
-    }
-    Admin.findById(req.session.user._id)
-      .then(user => {
-        req.user = user;
-        next();
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  });
-  app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    next();
-  });
-
-  // Vendor
-  app.use((req, res, next) => {
-    if(!req.session.user){
-      return next();
-    }
-    Vendor.findById(req.session.user._id)
-      .then(user => {
-        req.user = user;
-        next();
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  });
-  app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    next();
-  });
-
-// Global Variables
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-});
-
-
-app.use('/users', require('./routes/users'));
-app.use('/products', require('./routes/products'));
-app.use('/vendor', require('./routes/vendor'));
-app.use('/admin', require('./routes/admin'));
-app.use('/chat', require('./routes/chat'));
-
-
-
-*/
 const PORT = process.env.PORT || 3000;
 
 
